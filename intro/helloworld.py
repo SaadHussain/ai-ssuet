3e5467c537da372aa83af91b0ee56b79b5ccad11
Saad Hussain <saad.hussain@techlogix.com>
2017-10-29 00:34:20 +0500

    first project git desktop
    git desktop

---
+++ intro/helloworld.py
@@ -0,0 +1,12 @@
+"""bikes = []
+bikes.append('shaheen')
+bikes.append('hero')
+copy_of_bikes = bikes[:]
+print(bikes[0])
+print(copy_of_bikes)
+name = input("your name ?")
+print(name)"""
+
+fav_numbers = {'eric':17, 'edward':21}
+for name, number in fav_numbers.items():
+    print(name +' loves '+ str(number))
